8859541fec2fc66fbfe2fd0538c8275734841750
Cristian Sepulveda <cristiangsc@gmail.com>
2022-06-23 19:53:52 -0400

    Desafio 32 bit

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import games from './juegos.json'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    games
   },
   getters: {
+
   },
   mutations: {
   },
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Tienda 32 Bits</h1>
+    <h2>Lista de juegos</h2>
+    <table>
+      <thead>
+      <tr>
+        <th>Código</th>
+        <th>Nombre</th>
+        <th>Stock</th>
+        <th>Precio</th>
+      </tr>
+      </thead>
+      <tbody v-for="game in games" :key="game.codigo">
+      <tr :style="{color: game.color}">
+        <td>{{ game.codigo }}</td>
+        <td>{{ game.nombre }}</td>
+        <td>{{ game.stock }}</td>
+        <td>{{ game.precio }}</td>
+      </tr>
+      </tbody>
+    </table>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import {mapState} from 'vuex'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  computed: {
+    ...mapState(['games'])
+  },
 }
 </script>
+
+<style scoped>
+.container {
+  display: grid;
+  grid-template-columns: 800px;
+  justify-content: center;
+  width: 70%;
+  margin: 0 auto;
+}
+
+td{
+  border:2px solid black;
+}
+
+th{
+  border:2px solid black;
+}
+
+</style>
